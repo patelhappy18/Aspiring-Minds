5f008076c408fc25e19b8d4a5726e34b81b7084a
jainampatel <84738969+jainampatel@users.noreply.github.com>
2023-07-15 19:11:52 -0400

    Revert "Profile Page Initial Phase"

--- lib/index.dart
+++ lib/index.dart
@@ -1,9 +1,7 @@
-import 'package:aspirant_minds/courses.dart';
 import 'package:aspirant_minds/login.dart';
 import 'package:flutter/material.dart';
 import 'package:aspirant_minds/home.dart';
 import 'package:aspirant_minds/register.dart';
-import 'package:aspirant_minds/course_home_page.dart';
 
 class Index extends StatefulWidget {
   const Index({super.key});
