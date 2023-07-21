2f7fc73a5e137dfb646b26752078c74ebfb890c6
kushank patel <kushankpatel7@gmail.com>
2023-07-21 22:02:55 +0530

    Added module list component in CourseHomePage

--- lib/course_home_page.dart
+++ lib/course_home_page.dart
@@ -168,74 +168,74 @@ class _CourseHomePage extends State<CourseHomePage> {
                       ],
                     ),
 
-                    // Expanded(
-                    //   child: Container(
-                    //     decoration: BoxDecoration(
-                    //       color: const Color(0xFFF6F6F6),
-                    //       borderRadius: BorderRadius.circular(25),
-                    //     ),
-                    //     child: Padding(
-                    //       padding: const EdgeInsets.all(8),
-                    //       child: ListView.separated(
-                    //         itemCount: 25,
-                    //         separatorBuilder:
-                    //             (BuildContext context, int index) =>
-                    //                 const Divider(),
-                    //         itemBuilder: (BuildContext context, int index) {
-                    //           return Container(
-                    //             decoration: BoxDecoration(
-                    //               color: Colors.white,
-                    //               borderRadius: BorderRadius.circular(15),
-                    //             ),
-                    //             child: ExpansionTile(
-                    //               title: const Row(
-                    //                 mainAxisAlignment:
-                    //                     MainAxisAlignment.spaceBetween,
-                    //                 children: [
-                    //                   Text(
-                    //                     'Module 1',
-                    //                     style: TextStyle(
-                    //                       fontWeight: FontWeight.w800,
-                    //                     ),
-                    //                   ),
-                    //                   Text(
-                    //                     '\$\4',
-                    //                     style: TextStyle(
-                    //                       color: Color.fromRGBO(240, 130, 0, 1),
-                    //                       fontWeight: FontWeight.w800,
-                    //                     ),
-                    //                   ),
-                    //                 ],
-                    //               ),
-                    //               children: <Widget>[
-                    //                 const ListTile(
-                    //                   title: Text('1. Introduction'),
-                    //                 ),
-                    //                 const ListTile(
-                    //                   title: Text('2. What is UX/UI?'),
-                    //                 ),
-                    //                 const ListTile(
-                    //                   title: Text('3. Importance of is UX/UI?'),
-                    //                 ),
-                    //                 ElevatedButton(
-                    //                     onPressed: () {
-                    //                       onModulePurchase("module1");
-                    //                     },
-                    //                     style: ElevatedButton.styleFrom(
-                    //                       minimumSize:
-                    //                           const Size.fromHeight(40),
-                    //                       backgroundColor: const Color.fromRGBO(
-                    //                           240, 130, 0, 1),
-                    //                     ),
-                    //                     child: const Text("Purchase "))
-                    //               ],
-                    //             ),
-                    //           );
-                    //         },
-                    //       ),
-                    //     ),
-                    //   ),
-                    // ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFF6F6F6),
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: ListView.separated(
+                            itemCount: 25,
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    const Divider(),
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: ExpansionTile(
+                                  title: const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Module 1',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w800,
+                                        ),
+                                      ),
+                                      Text(
+                                        '\$\4',
+                                        style: TextStyle(
+                                          color: Color.fromRGBO(240, 130, 0, 1),
+                                          fontWeight: FontWeight.w800,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  children: <Widget>[
+                                    const ListTile(
+                                      title: Text('1. Introduction'),
+                                    ),
+                                    const ListTile(
+                                      title: Text('2. What is UX/UI?'),
+                                    ),
+                                    const ListTile(
+                                      title: Text('3. Importance of is UX/UI?'),
+                                    ),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          onModulePurchase("module1");
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          minimumSize:
+                                              const Size.fromHeight(40),
+                                          backgroundColor: const Color.fromRGBO(
+                                              240, 130, 0, 1),
+                                        ),
+                                        child: const Text("Purchase "))
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
