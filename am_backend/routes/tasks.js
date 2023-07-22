ade97ec29039af9070d009d5810fceb6938e76ef
shaily5 <shailyp05@gmail.com>
2023-07-22 00:41:42 -0700

    added route for search users API

--- am_backend/routes/tasks.js
+++ am_backend/routes/tasks.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-
-const passport = require('passport');
+const passport = require("passport");
 const {
-
   createUser,
-  getUser,emailSend,changePassword,updatePasswordGoogle
-  
+  getUser,
+  emailSend,
+  changePassword,
+  updatePasswordGoogle,
+  searchUsers,
 } = require("../controllers/register");
 
 router.route("/registerNewUser").post(createUser);
 
 router.route("/getUser").post(getUser);
 
+router.route("/searchUsers").get(searchUsers);
+
 router.route("/email-send").post(emailSend);
 
 router.route("/change-password").post(changePassword);
 
 router.route("/update-password-google").post(updatePasswordGoogle);
-
 
 // router.route("/fetchuser").post(createUser);
 
